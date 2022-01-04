ddb46459e7108ea3ce24db2876f6e1c25aef51fd
donnalynp22 <donnalynp22@hotmail.com>
2022-01-04 15:54:14 +0000

    Created service class and service methods interface

---
+++ SpringDrinks/src/main/java/com/qa/SpringDrinks/service/FizzyDrinkService.java
@@ -0,0 +1,42 @@
+package com.qa.SpringDrinks.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.qa.SpringDrinks.domain.FizzyDrink;
+
+@Service
+public class FizzyDrinkService implements ServiceMethods<FizzyDrink>{
+
+	@Override
+	public FizzyDrink create(FizzyDrink t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<FizzyDrink> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public FizzyDrink getById(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public FizzyDrink update(long id, FizzyDrink t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean delete(long id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
